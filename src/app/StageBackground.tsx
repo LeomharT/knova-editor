e8e6591c9948b62399ea1d0d50f1bda50f30e8d6
LeomharT <leo.liao.lzy@gmail.com>
2025-12-12 21:20:54 +0800

    feat: ui

--- src/app/StageBackground.tsx
+++
@@ -1,3 +0,0 @@
-export default function StageBackground() {
-  return <div>StageBackground</div>;
-}
--- src/component/Toolbar/idnex.tsx
+++ src/component/Toolbar/idnex.tsx
@@ -1,10 +1,30 @@
 import { Button, Card, Divider, Space, Tooltip } from 'antd';
 
-import { IconLocation, IconLock, IconSquare } from '@tabler/icons-react';
+import { IconLocation, IconLock, IconRoute, IconSquare } from '@tabler/icons-react';
 import { useState } from 'react';
 import classes from './style.module.css';
 export default function Toolbar() {
   const [active, setActive] = useState('');
+
+  const [lock, setLock] = useState(false);
+
+  const actions = [
+    {
+      key: 'cursor',
+      tooltip: 'Cursor',
+      icon: <IconLocation style={{ transform: 'rotate(-90deg) translateX(-1px)' }} />,
+    },
+    {
+      key: 'square',
+      tooltip: 'Square',
+      icon: <IconSquare />,
+    },
+    {
+      key: 'connect',
+      tooltip: 'Connect',
+      icon: <IconRoute />,
+    },
+  ];
 
   return (
     <div className={classes.root}>
@@ -12,30 +32,24 @@ export default function Toolbar() {
         <Space>
           <Tooltip title='Lock select'>
             <Button
-              type={active === 'lock' ? 'primary' : 'text'}
+              type={lock ? 'primary' : 'text'}
               size='large'
               icon={<IconLock />}
-              onClick={() => setActive('lock')}
+              onClick={() => setLock((prev) => !prev)}
             />
           </Tooltip>
           <Divider vertical />
           <Space size='middle'>
-            <Tooltip title='Cursor'>
-              <Button
-                type={active === 'cursor' ? 'primary' : 'text'}
-                size='large'
-                icon={<IconLocation style={{ transform: 'rotate(-90deg) translateX(-1px)' }} />}
-                onClick={() => setActive('cursor')}
-              />
-            </Tooltip>
-            <Tooltip title='Square'>
-              <Button
-                type={active === 'rect' ? 'primary' : 'text'}
-                size='large'
-                icon={<IconSquare />}
-                onClick={() => setActive('rect')}
-              />
-            </Tooltip>
+            {actions.map((item) => (
+              <Tooltip key={item.key} title={item.tooltip}>
+                <Button
+                  size='large'
+                  type={active === item.key ? 'primary' : 'text'}
+                  icon={item.icon}
+                  onClick={() => setActive(item.key)}
+                />
+              </Tooltip>
+            ))}
           </Space>
         </Space>
       </Card>
